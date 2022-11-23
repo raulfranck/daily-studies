c8214e8beeb703516a02ed896d1ac56b5d4bbc08
raulfranck <raulengfranck@gmail.com>
2022-11-22 21:52:02 -0300

    Revendo alguns conceitos de Observables e Interval

---
+++ rxjs/observable/ex02.js
@@ -0,0 +1,21 @@
+import { Observable } from "rxjs";
+
+const obs$ = new Observable(subs => {
+    subs.next('RxJS')
+    subs.next('teste')
+    subs.next('teste1')
+    subs.next('teste2')
+
+    if(Math.random() > 0.5) {
+        throw "Chamando erro"
+    } else {
+        subs.complete()
+    }
+    
+})
+
+obs$.subscribe(
+    text => console.log(text),
+    err => console.log(err),
+    () => console.log('fim')
+    )
